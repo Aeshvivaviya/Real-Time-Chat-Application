5421d57a7815b4d14a5cbd45cfc2e59b2eb255df
Aeshvi Vaviya <aeshvi@gmail.com>
2026-03-23 14:33:59 +0530

    Switched to HashRouter to fix 404 /chat issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './components/Login';
 import Chat from './components/Chat'; // ✅ Make sure this path is correct
 import { generateToken } from "./utils/getToken";
